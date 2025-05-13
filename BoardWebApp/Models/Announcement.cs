9c75717e43005b6ecda9a8586ffe295ecfe2c4dd
Ace Danger <hpu.presence@gmail.com>
2025-05-13 12:30:45 +0300

    Fixed minor bugs

--- BoardWebApp/Models/Announcement.cs
+++ BoardWebApp/Models/Announcement.cs
@@ -1,13 +1,24 @@
-﻿namespace BoardWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoardWebApp.Models
 {
     public class Announcement
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100)]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
         public DateTime CreatedDate { get; set; }
         public bool Status { get; set; }
+
+        [Required]
         public string Category { get; set; }
+
+        [Required]
         public string SubCategory { get; set; }
     }
 }
